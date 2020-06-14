920b8a15fd513d716dccb45b02b370c51bf48adf
Niels Dyrberg <ncpd@live.dk>
2020-06-14 18:27:18 +0200

    Initial commit

---
+++ top.vhd
@@ -0,0 +1,26 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.all;
+
+entity top is
+	port(
+
+		clk											: in std_logic
+	);
+
+end top;
+
+architecture rtl of top is
+	
+
+
+begin
+
+
+	
+end rtl;
+
+
+
+
