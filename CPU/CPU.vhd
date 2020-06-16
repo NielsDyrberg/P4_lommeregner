47d7ebd688140187e06962a6a582a184676fbe97
Niels Dyrberg <ncpd@live.dk>
2020-06-16 03:17:10 +0200

    All ports are now added to the CU entity. The data_out_cu port is still not removed.; .resting. stuff between every stage also works; now data_out_cu will be removed, and then the reset stages will be added.

--- CPU/CPU.vhd
+++ CPU/CPU.vhd
@@ -24,13 +24,26 @@ end CPU;
 
 architecture rtl of CPU is
 
+	signal instrReg_cpu	:	std_logic_vector(19 downto 0) := (others => '0');
+	signal status_cpu		:	std_logic_vector(4 downto 0)	:= (others => '0');
+
 	component cu
 		port(
-				clk_cu		: in	std_logic;
-				reset_cu	 	: in	std_logic;
-				ready_cu		: in  std_logic;
+				clk_cu								: in	std_logic;
+				reset_cu	 							: in	std_logic;
+				ready_cu								: in  std_logic;
 				
-				data_out_cu	: out	std_logic_vector(2 downto 0)
+				data_out_cu							: out	std_logic_vector(2 downto 0);
+				
+				rw_cu, vma_cu, opregSel_cu, 
+				errorSel_cu, addrSel_cu, 
+				instrSel_cu, oe_cu				: out std_logic;
+				outSel_cu							: out std_logic_vector(1 downto 0);
+				regSel_cu							: out std_logic_vector(4 downto 0); 
+				shiftSel_cu							: out std_logic_vector(2 downto 0);
+				aluSel_cu							: out std_logic_vector(3 downto 0);
+				status_cu							: in 	std_logic_vector(4 downto 0);
+				instrReg_cu							: in 	std_logic_vector(19 downto 0)
 			);
 	end component;
 
@@ -40,7 +53,21 @@ begin
 									reset_cu		=> reset_cpu,	
 									ready_cu		=> ready_cpu,
 
-									data_out_cu	=> data_out_cpu
+									data_out_cu	=> data_out_cpu,
+									
+									rw_cu			=> open, 
+									vma_cu		=> open, 
+									opregSel_cu	=> open, 
+									errorSel_cu	=> open, 
+									addrSel_cu	=> open, 
+									instrSel_cu	=> open, 
+									oe_cu			=> open,
+									outSel_cu	=> open,
+									regSel_cu	=> open,
+									shiftSel_cu	=> open,
+									aluSel_cu	=> open,
+									status_cu	=> status_cpu,
+									instrReg_cu	=> instrReg_cpu
 								);
 	
 
--- CPU/CU/CU.vhd
+++ CPU/CU/CU.vhd
@@ -17,7 +17,17 @@ entity CU is
 		reset_cu	 	: in	std_logic;
 		ready_cu		: in  std_logic;
 		
-		data_out_cu	: out	std_logic_vector(2 downto 0)
+		data_out_cu	: out	std_logic_vector(2 downto 0);
+		
+		rw_cu, vma_cu, opregSel_cu, 
+		errorSel_cu, addrSel_cu, 
+		instrSel_cu, oe_cu				: out std_logic;
+		outSel_cu							: out std_logic_vector(1 downto 0);
+		regSel_cu							: out std_logic_vector(4 downto 0); 
+		shiftSel_cu							: out std_logic_vector(2 downto 0);
+		aluSel_cu							: out std_logic_vector(3 downto 0);
+		status_cu							: in 	std_logic_vector(4 downto 0);
+		instrReg_cu							: in 	std_logic_vector(19 downto 0)
 	);
 	
 end entity;
@@ -40,28 +50,36 @@ begin
 			case state is
 				when s0=>
 						state <= s1;
+						
 				when s1=>
 					state <= s2;
+					
 				when s2=>
 					state <= s3;
+					
 				when s3 =>
 					if ready_cu = '1' then
 						state <= s4;
 					else
 						state <= s3;
 					end if;
+					
 				when s4 =>
 					state <= s5;
+					
 				when s5 =>
 					state <= s6;
+					
 				when s6 =>
 					state <= s7;
+					
 				when s7 =>
 					if ready_cu = '1' then
 						state <= s0;
 					else
 						state <= s7;
 					end if;
+					
 			end case;
 		end if;
 	end process;
@@ -69,24 +87,46 @@ begin
 	-- Output depends solely on the current state
 	process (state)
 	begin
-	
+		oe_cu						<= '0';	--1 data_in <= data_out, 0 data_in <= ZZ
+		addrSel_cu 				<= '0';	-- 0 == NA, 1 == wr
+		outSel_cu				<= "00"; -- (en, rw)
+		opregSel_cu				<= '0'; 	-- (rd, wr)
+		instrSel_cu				<= '0'; 	-- 0 == NA, 1 == wr
+		rw_cu						<= '1';	-- 1 == rd, 0 == wr 	
+		vma_cu					<= '0';
+		errorSel_cu				<= '0';	--0 == No error, 1 == Si Error
+		regSel_cu				<= "00000";
+		shiftSel_cu 			<= shiftpass;
+		aluSel_cu				<= alupass;
+		data_out_cu 			<= "000";
+		
 		case state is
 			when s0 =>
 				data_out_cu <= "000";
+				
 			when s1 =>
 				data_out_cu <= "001";
+				
 			when s2 =>
 				data_out_cu <= "010";
+				oe_cu			<= '1';
+				
 			when s3 =>
 				data_out_cu <= "011";
+				
 			when s4 =>
 				data_out_cu	<= "100";
+				
 			when s5 =>
 				data_out_cu	<= "101";
+				
 			when s6 =>
 				data_out_cu	<= "110";
+				
 			when s7 =>
 				data_out_cu	<= "111";
+				aluSel_cu	<=	"0001";
+				
 		end case;
 	end process;
 	
