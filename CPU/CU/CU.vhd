6c8ac11710fd32b913c43babf473532e180ccdf1
Niels Dyrberg <ncpd@live.dk>
2020-06-16 03:43:20 +0200

    The whole restart process works. I am considering ceeping the data_out_cu but renaming it to something like state_cnt_cu for debuggin purposes.. i am wondering if the state machine will work after a rename.

--- CPU/CU/CU.vhd
+++ CPU/CU/CU.vhd
@@ -49,36 +49,34 @@ begin
 		elsif (rising_edge(clk_cu)) then
 			case state is
 				when s0=>
-						state <= s1;
+					state <= s1;
 						
 				when s1=>
 					state <= s2;
 					
 				when s2=>
+
 					state <= s3;
 					
 				when s3 =>
-					if ready_cu = '1' then
-						state <= s4;
-					else
-						state <= s3;
-					end if;
+					
+					state <= s4;
 					
 				when s4 =>
 					state <= s5;
 					
 				when s5 =>
-					state <= s6;
+					if ready_cu = '1' then
+						state			<= s6;
+					else
+						state			<= s5;
+					end if;
 					
 				when s6 =>
 					state <= s7;
 					
 				when s7 =>
-					if ready_cu = '1' then
 						state <= s0;
-					else
-						state <= s7;
-					end if;
 					
 			end case;
 		end if;
@@ -105,27 +103,40 @@ begin
 				data_out_cu <= "000";
 				
 			when s1 =>
+				aluSel_cu 					<= aluZero;
 				data_out_cu <= "001";
 				
 			when s2 =>
+				aluSel_cu 					<= aluZero;
+				outSel_cu					<= wr;
 				data_out_cu <= "010";
-				oe_cu			<= '1';
 				
 			when s3 =>
+				outSel_cu					<= rd;
+				oe_cu							<= '1';
 				data_out_cu <= "011";
 				
 			when s4 =>
+				outSel_cu					<= rd;
+				oe_cu							<= '1';
+				regSel_cu(4 downto 3)	<= wr;
+				regSel_cu(2 downto 0)	<= progReg;
+				addrSel_cu					<=	'1';
+				opregSel_cu					<= '1';
 				data_out_cu	<= "100";
 				
 			when s5 =>
+				vma_cu						<= '1';
 				data_out_cu	<= "101";
 				
 			when s6 =>
+				vma_cu						<= '1';
+				instrSel_cu 				<=	'1';
 				data_out_cu	<= "110";
 				
 			when s7 =>
+			
 				data_out_cu	<= "111";
-				aluSel_cu	<=	"0001";
 				
 		end case;
 	end process;
